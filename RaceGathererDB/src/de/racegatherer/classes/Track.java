2eb44c6beda06317383626a387347f8a22f40f99
padde <padde@earl>
2012-06-27 13:02:32 +0200

    track class added

---
+++ RaceGathererDB/src/de/racegatherer/classes/Track.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.racegatherer.classes;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author padde
+ */
+@Entity
+public class Track {
+    
+}
